606655fd22e0f6205bcf16517f8b2efb34bb4cb0
Arnstein Løkling <arnstein.lokling@eika.no>
2024-05-27 18:25:12 +0200

    fix: chapter 12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -150,6 +150,7 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
+  console.log(id);
   try {
     const data = await sql<InvoiceForm>`
       SELECT
